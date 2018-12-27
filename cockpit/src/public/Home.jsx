aa01b1dba90f621f8d801e6bd882a369bdb4a4a8
Muyinda Rogers <moverr@gmail.com>
2018-12-27 19:46:01 +0300

    :init home page :

---
+++ cockpit/src/public/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../css/home.css';
+class Home extends Component {
+    render() {
+        return (
+            <div className={'home'}>
+                <h1>SCHOLA </h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
