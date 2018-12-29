af40f4452f44c8ed1be8b8ff11ef75777a375d73
Muyinda Rogers <moverr@gmail.com>
2018-12-29 04:00:58 +0300

    :Debugging . :

--- cockpit/src/components/Login.jsx
+++ cockpit/src/components/Login.jsx
@@ -2,10 +2,19 @@ import React, { Component } from 'react'
 import '../css/home.css';
 
 class Login extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value:''
+        }
+    }
+    
     render() {
         return (
             <div className="body-wrapper">
-                <h1>LOGIN MOVER</h1>
+                <h1>Enter Shoool Name</h1>
+                <input type={'text'} value={this.state.value} />
             </div>
         )
     }
