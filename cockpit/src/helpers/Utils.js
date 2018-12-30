68e4078ce58f0e3ab0ac5409e6d9e37c40e1901e
Muyinda Rogers <moverr@gmail.com>
2018-12-30 13:08:54 +0300

    :Debugging :

--- cockpit/src/helpers/Utils.js
+++ cockpit/src/helpers/Utils.js
@@ -1,14 +1,22 @@
 class Utils{
 
      BASE_URL = " http://localhost:9876/"; 
-    
-
-     
+     endpoints  = {
+         users:"user/v1/",
+         roles:"roles/v1/",
+         
+         
+     }
 
      getBaseUrl(){
         return this.BASE_URL;
     }
 
+    getEndPoint(endpoint){
+        return this.endpoints.endpoint;
+    }
+
+    
 
     
     
