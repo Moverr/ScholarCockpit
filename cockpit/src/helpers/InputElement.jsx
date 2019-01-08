6fb043e9001b5d9a76a9f524ee2e2a20b31cb872
Muyinda Rogers <moverr@gmail.com>
2019-01-08 10:27:36 +0300

    :Stateless Components :

--- cockpit/src/helpers/InputElement.jsx
+++ cockpit/src/helpers/InputElement.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
-class Inputs extends Component {
+//This is a stateless component
+class InputElement extends Component {
 	render() {
 		const { username, callback, name, type, className } = this.props;
 		return <input name={name} type={type} className={className} value={username} onChange={callback} />;
 	}
 }
 
-export default Inputs;
+export default InputElement;
