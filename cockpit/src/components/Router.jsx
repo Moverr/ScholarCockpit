d66abd1ef122c11e55ac1dc21ccd8abd0a4a72bf
Muyinda Rogers <moverr@gmail.com>
2019-01-08 15:07:30 +0300

    :debugging :

--- cockpit/src/components/Router.jsx
+++ cockpit/src/components/Router.jsx
@@ -1,31 +1,24 @@
 import React, { Component } from 'react';
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Login from './Login';
 import Home from './Home';
 import Verify from './Verify';
 // import Verify from './Verify';
 
-
- 
-
 class Router extends Component {
-    render() {
-        return (
-            <div>
-                <Switch>    
-                    <Route exact path="/" component={Home} />                  
-                    <Route exact path="/home" component={Home} />                              
-                    <Route exact path="/login/:schoolName" component={Login} />   
-                    <Route exact path="/verify" component={Verify} />   
-                     
-                    
-                                      
-                 </Switch>
-                 
-                
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/home" component={Home} />
+					<Route exact path="/login/:schoolName" component={Login} />
+					<Route exact path="/verify" component={Verify} />
+					<Route exact path="/login" component={Verify} />
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default Router;
