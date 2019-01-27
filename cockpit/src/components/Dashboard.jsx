abc9a9990a71513885ecb8b2c67ae155f9eb938c
Muyinda Rogers <moverr@gmail.com>
2019-01-27 15:19:56 +0300

    Dashboard : debugging :

--- cockpit/src/components/Dashboard.jsx
+++ cockpit/src/components/Dashboard.jsx
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import { verifySchool, pushHistory, verifyAuthentication } from '../helpers/Utils';
 
 class Dashboard extends Component {
-    render() {
-        return (
-            <div>
-                <h1> Welcome to the Dashboard </h1>
-            </div>
-        );
-    }
+	componentDidMount() {
+		let schoolName = verifySchool();
+		let authentication = verifyAuthentication();
+
+		if (schoolName === null) {
+			let path = `/verify/`;
+			pushHistory(path, this.props);
+			return;
+		}
+
+		if (authentication === null) {
+			let path = `/login/` + schoolName;
+			pushHistory(path, this.props);
+			return;
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<h1> Welcome to the Dashboard </h1>
+			</div>
+		);
+	}
 }
 
 export default Dashboard;
