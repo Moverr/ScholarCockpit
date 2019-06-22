b44267a3ba0a8184c3879558866e26b1a2756fb1
Rodgers Muyinda <rmuyinda@cellulant.com>
2019-06-23 01:29:12 +0300

    LDebugging

--- src/utils/Loggin.js
+++ src/utils/Loggin.js
@@ -1,14 +1,15 @@
 
-function logging(message = null){
+export function logging(message = null){
     if(message != null){
         console.log(message);
     }
 }
 
-function loggingTable(section,message,params){
+export function loggingTable(section,message,params){
     console.table({
         "SECTION":section,
         "MESSAGE":message,
         "PARAMS" : params
     })
 }
+
