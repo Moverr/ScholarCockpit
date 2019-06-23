640eb64f340a424c0ffee93c9bc4f0ab6d486ee6
Rodgers Muyinda <rmuyinda@cellulant.com>
2019-06-23 03:07:38 +0300

    :Debuging :

--- src/components/public/HeaderBar.jsx
+++ src/components/public/HeaderBar.jsx
@@ -57,45 +57,12 @@ class HeaderBar extends Component {
               <li className="active">
                 <a className="dropdown-toggle" data-toggle="dropdown" href="components.html">Components</a>
                 <ul className="dropdown-menu">
-                  <li><a href="components.html">Overview</a></li>
-                  <li><a href="components.html#glyphicons">Glyphicons</a></li>
-                  <li><a href="components.html#dropdowns">Dropdowns</a></li>
-                  <li><a href="components.html#btn-groups">Button groups</a></li>
-                  <li><a href="components.html#btn-dropdowns">Button dropdowns</a></li>
-                  <li><a href="components.html#input-groups">Input groups</a></li>
-                  <li><a href="components.html#nav">Navs</a></li>
-                  <li><a href="components.html#navbar">Navbar</a></li>
-                  <li><a href="components.html#breadcrumbs">Breadcrumbs</a></li>
-                  <li><a href="components.html#pagination">Pagination</a></li>
-                  <li><a href="components.html#labels">Labels</a></li>
-                  <li><a href="components.html#badges">Badges</a></li>
-                  <li><a href="components.html#jumbotron">Jumbotron</a></li>
-                  <li><a href="components.html#page-header">Page header</a></li>
-                  <li><a href="components.html#thumbnails">Thumbnails</a></li>
-                  <li><a href="components.html#alerts">Alerts</a></li>
-                  <li><a href="components.html#progress">Progress bars</a></li>
-                  <li><a href="components.html#media">Media object</a></li>
-                  <li><a href="components.html#list-group">List group</a></li>
-                  <li><a href="components.html#panels">Panels</a></li>
-                  <li><a href="components.html#responsive-embed">Responsive embed</a></li>
                   <li><a href="components.html#wells">Wells</a></li>
                 </ul>
               </li>
               <li>
                 <a className="dropdown-toggle" data-toggle="dropdown" href="javascript.html">JavaScript</a>
                 <ul className="dropdown-menu">
-                  <li><a href="javascript.html#js-overview">Overview</a></li>
-                  <li><a href="javascript.html#transitions">Transitions</a></li>
-                  <li><a href="javascript.html#modals">Modal</a></li>
-                  <li><a href="javascript.html#dropdowns">Dropdown</a></li>
-                  <li><a href="javascript.html#scrollspy">Scrollspy</a></li>
-                  <li><a href="javascript.html#tabs">Tab</a></li>
-                  <li><a href="javascript.html#tooltips">Tooltip</a></li>
-                  <li><a href="javascript.html#popovers">Popover</a></li>
-                  <li><a href="javascript.html#alerts">Alert</a></li>
-                  <li><a href="javascript.html#buttons">Button</a></li>
-                  <li><a href="javascript.html#collapse">Collapse</a></li>
-                  <li><a href="javascript.html#carousel">Carousel</a></li>
                   <li><a href="javascript.html#affix">Affix</a></li>
                 </ul>
               </li>
