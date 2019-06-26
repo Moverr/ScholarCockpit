6a51b2a5e171aca91f9e8112826516bf47f136cb
Rodgers Muyinda <rmuyinda@cellulant.com>
2019-06-26 17:01:43 +0300

    : git status

--- src/components/public/Home.jsx
+++ src/components/public/Home.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import '../../css/home.css';
 
-import  HeaderBar  from  './HeaderBar';
+import  TopNavigation  from  './TopNavigation';
 
 
 
@@ -10,7 +10,7 @@ class Home extends Component {
         return (
 
             <div >
-                <HeaderBar/>
+                <TopNavigation/>
 
                      
                 <div className={"row"}>
