1e8e7b7f52a8db6da7e99f17c3b2ed4b6d631a6b
Rodgers Muyinda <rmuyinda@cellulant.com>
2019-06-27 21:26:32 +0300

    :Debugging, The Lok and Feel :

--- src/components/public/HeaderBar.jsx
+++ src/components/public/HeaderBar.jsx
@@ -5,25 +5,35 @@ import { applicationTitle } from '../../utils/Constants';
 import logo from '../../images/logo.png';
 
 import '../../css/nav.css';
+import Banner from './Banner';
 
 const logdata = (
-    {
-      "section": "HeaderBar",
-      "message": "Layout to the header section ",
-      "props": "N/A",
+  {
+    "section": "HeaderBar",
+    "message": "Layout to the header section ",
+    "props": "N/A",
 
-    }
-  );
-  
-function  HeaderBar () {
-    logTable(logdata);
-     
-    return (
-      <div classname={"row-fluid"}>
-       <h1> Header Section </h1>
+  }
+);
+
+function HeaderBar() {
+  logTable(logdata);
+
+  return (
+    <div className={"row-fluid"}>
+      <div className="row headerbar">
+        <div className="col-md-8">
+           <Banner/>
+        </div>
+        <div className="col-md-4">
+
+          <h1>Sing ooff </h1>
+        </div>
       </div>
 
-    )
-  
+    </div>
+
+  )
+
 }
 export default HeaderBar;
