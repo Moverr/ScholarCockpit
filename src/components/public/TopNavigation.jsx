499ade55b3e6adfa602fb92ced9ec88c4ffb6edb
Rodgers Muyinda <rmuyinda@cellulant.com>
2019-06-28 14:51:19 +0300

    refactory, Top Navigation UI :

--- src/components/public/TopNavigation.jsx
+++ src/components/public/TopNavigation.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom';
 import { logTable } from '../../utils/Logger';
@@ -7,40 +8,48 @@ import logo from '../../images/logo.png';
 import '../../css/nav.css';
 
 const logdata = (
-    {
-      "section": "TopNavigation",
-      "message": "Layout the public menu and navigation ",
-      "props": "N/A",
+  {
+    "section": "TopNavigation",
+    "message": "Layout the public menu and navigation ",
+    "props": "N/A",
 
-    }
-  );
-  
-function  TopNavigation () {
-    logTable(logdata);
-     
-    return (
-      <div classname={"row-fluid"}>
-        <nav id="bs-navbar" className=" col-md-12 navbar navbar-fixed-top  navbar-expand-lg navbar-light ">
-          <ul className="nav navbar-nav">
-            <li><img src={logo} alt="Logo" /> <NavLink className={"navbar-brand"} to={'#'} activeClassName="active"> {applicationTitle} </NavLink>
-            </li>
-          </ul>
+  }
+);
 
 
-          <ul className="nav  navigation_right ">
-            <li> <NavLink to={'#'} activeClassName="active"> Product &nbsp; | &nbsp;</NavLink> 
-            </li> 
-            <li> <NavLink to={'#'} activeClassName="active"> Pricing &nbsp; | &nbsp;</NavLink> 
-            </li> 
-            <li> <NavLink to={'#'} activeClassName="active"> Support &nbsp;</NavLink> 
-            </li> 
-            
-          </ul>
+function TopNavigation() {
+  logTable(logdata);
 
-        </nav>
-      </div>
+  return (
+    <div classname={"row-fluid"}>
+      <nav id="bs-navbar" className=" col-md-12 navbar navbar-fixed-top  navbar-expand-lg navbar-light ">
+        <ul className="nav navbar-nav">
+          <li><img src={logo} alt="Logo" /> <NavLink className={"navbar-brand"} to={'#'} activeClassName="active"> {applicationTitle} </NavLink>
+          </li>
+        </ul>
+        {navigationPanel()}
+ 
+      </nav>
+    </div>
 
-    )
-  
+  )
+
 }
+
+
+function navigationPanel() {
+  return <ul className="nav  navigation_right ">
+    <li> <NavLink to={'#'} activeClassName="active"> Product &nbsp; | &nbsp;</NavLink>
+    </li>
+    <li> <NavLink to={'#'} activeClassName="active"> Pricing &nbsp; | &nbsp;</NavLink>
+    </li>
+    <li> <NavLink to={'#'} activeClassName="active"> Support &nbsp;</NavLink>
+    </li>
+
+  </ul>;
+}
+
+
+
 export default TopNavigation;
+
