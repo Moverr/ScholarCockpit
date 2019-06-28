6518a824573e59b159c989d6bf62fa30bce9a938
Rodgers Muyinda <rmuyinda@cellulant.com>
2019-06-29 02:20:35 +0300

    Verify School View :

--- src/components/public/VerifySchool.jsx
+++ src/components/public/VerifySchool.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
 
 function VerifySchool() {
-    
+   return  <div className="verifySchoolview">
+         <br />
+          <h5>Enter School Name</h5>
+          <input type="text" className="text" value="" placeholder="Enter School Name " />
+          <br />
+          
+          <button>CONTINUE</button>
+    </div>
 }
 
 export default VerifySchool;
