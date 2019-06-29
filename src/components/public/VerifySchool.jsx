536d73401cf4a7286bbaf96ca40c6ce97849c01b
Rodgers Muyinda <rmuyinda@cellulant.com>
2019-06-29 08:33:36 +0300

    :Debugging :

--- src/components/public/VerifySchool.jsx
+++ src/components/public/VerifySchool.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-
+import InputField from '../../forms/inputs/InputField';
 function VerifySchool() {
    return  <div className="verifySchoolview col-md-12">
          <br />
           <h5>Enter School Name</h5>
-          <input type="text" className="text" value="" placeholder="Enter School Name " />
+          <InputField type="text" className="text" value="" placeholder="Enter School Name " />
+           
           <br />
           
           <button>CONTINUE</button>
