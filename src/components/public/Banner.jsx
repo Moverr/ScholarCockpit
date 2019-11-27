322fa86b6478cb21b5204683a6aa4170049ca890
Muyinda Rogers <moverr@gmail.com>
2019-11-27 14:23:36 +0300

    Re-designing the landing page :

--- src/components/public/Banner.jsx
+++ src/components/public/Banner.jsx
@@ -6,7 +6,7 @@ import { logTable } from '../../utils/Logger';
 function Banner() {
  
   return (
-    <div  className="banner">
+    <div  className="banner  col-md-12">
       <h1 > SCHOOL MANAGEMENT SYSTEM <br/> v1.0 beta</h1>
     </div>
 
