9e380d60619ed36d345809b88ec54269c64c168b
Muyinda Rogers <moverr@gmail.com>
2019-11-28 06:27:50 +0300

    :Debugging :

--- src/components/public/Banner.jsx
+++ src/components/public/Banner.jsx
@@ -7,7 +7,7 @@ function Banner() {
  
   return (
     <div  className="banner  col-md-12">
-      <h1 > SCHOLAR   </h1>
+      <h1 className="col-md-12"> SCHOLAR   </h1>
       School Management System  v1.0 beta
     </div>
 
