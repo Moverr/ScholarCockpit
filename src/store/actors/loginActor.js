1400250f19590bcddcf718a9304fac02ae35ba4e
Muyinda Rogers <moverr@gmail.com>
2019-12-12 20:01:20 +0300

    Login Actour

---
+++ src/store/actors/loginActor.js
@@ -0,0 +1,13 @@
+export const loggin =(payload)=>{
+    return {
+        type:"LOGIN_in",
+        data:payload
+    }
+}
+
+export const logout=(payload)=>{
+    return {
+        type:"LOGIN_in",
+        data:payload
+    }
+}
