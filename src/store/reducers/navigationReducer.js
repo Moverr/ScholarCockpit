ba6c1c3b19de6061df44fa6570f56e17ed6374f8
Muyinda Rogers <moverr@gmail.com>
2020-01-06 11:49:06 +0300

    Navigation Reducer :

---
+++ src/store/reducers/navigationReducer.js
@@ -0,0 +1,51 @@
+ 
+let defaultAuthentication = {
+    schoolname: null,
+    authentication: null
+};
+
+let AuthenticationStatus = {
+    status: "",
+    message: "",
+
+}
+
+
+
+export const AuthenticationReducer = (state = defaultAuthentication, action) => {
+    switch (action.type) {
+        case 'ADD_SCHOOLNAME':
+
+            state = { ...state, schoolname: action.payload };
+
+            break;
+        case 'ADD_AUTHENTICATION':
+            state = { ...state, authentication: action.payload };
+            break;
+
+
+        default:
+            break;
+    }
+    return state;
+};
+
+
+
+export const LoginResponseReducer = (state = AuthenticationStatus, action) => {
+    switch (action.type) {
+        case "LOGIN_FAILURE":
+            state = { ...state, message: action.payload }
+            break;
+
+        case "LOGIN_STATUS":
+            state = { ...state, status: action.payload }
+            break;
+
+        default:
+            break;
+    }
+    return state;
+};
+
+
