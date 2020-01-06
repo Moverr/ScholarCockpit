d4798f27effd3b42277b8570f507631e5a877baa
Muyinda Rogers <moverr@gmail.com>
2020-01-06 16:01:25 +0300

    :Debugging :

--- src/components/public/RightBar.jsx
+++ src/components/public/RightBar.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 import { logTable } from '../../utils/Logger';
 
 
 import '../../css/nav.css';
 import Banner from './Banner';
 import VerifySchool from './VerifySchool';
-import Login  from './Login';
-const screennavigation = useSelector(state => state.navigation);
-      
+import Login from './Login';
+
+
 
 
 const logdata = (
@@ -19,16 +21,40 @@ const logdata = (
   }
 );
 
+const renderSwitch = (level) => {
+
+  switch (level) {
+    case 'LOGIN':
+      break;
+    case 'VERIFYSCHOOL':
+      break;
+
+    case 'FORGOT_PASSWORD':
+      break;
+
+  }
+}
+
 function RightBar(props) {
+
+  const screennavigation = useSelector(state => state.navigation);
 
   logTable(logdata);
   const urlsegment = props.urlSegment;
   console.log("SOEOEOEOEOEOEO");
   console.log(props);
 
+
   return (
     <div className={"row-fluid"}>
       <div className="row headerbar">
+        {
+          switch(screennavigation){
+            case: "LOGIN":
+          dbreak;
+
+      }
+    }
         <div className="col-md-9">
           <Banner props={props} />
         </div>
