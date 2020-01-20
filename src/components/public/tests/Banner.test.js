d0a700ab5064e89ce6d5f948d92e45f9e1f9944d
Muyinda Rogers <moverr@gmail.com>
2020-01-20 19:31:38 +0300

    :Debugging

--- src/components/public/tests/Banner.test.js
+++ src/components/public/tests/Banner.test.js
@@ -2,28 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import Banner from '../Banner';
- 
- 
 
 
-      
- let wrapper = shallow(<Banner />);
 
-   
+
+
+let wrapper = shallow(<Banner />);
+
+
 
 describe('Testing Input FIeld', () => {
-     
 
-  it('should  render correctly', () => {       
 
-      expect(wrapper).toMatchSnapshot();
+  it('should  render correctly', () => {
+    expect(wrapper).toMatchSnapshot();
   });
 
-  it('Should have a class banner',()=>{
-        expect(wrapper).toMatchSnapshot();
+  it('Should have a class banner', () => {
+    expect(wrapper).toMatchSnapshot();
   });
 })
 
 
 
- 
--- src/components/public/tests/VerifySchool.test.js
+++ src/components/public/tests/VerifySchool.test.js
@@ -2,26 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import VerifySchool from '../VerifySchool';
- 
- 
 
 
-      
- let wrapper = shallow(<VerifySchool />);
 
-   
+
+
+let wrapper = shallow(<VerifySchool />);
+
+
 
 describe('Testing Input FIeld', () => {
-     
-
-  it('should  render correctly', () => {       
-
-      expect(wrapper).toMatchSnapshot();
+  it('should  render correctly', () => {
+    expect(wrapper).toMatchSnapshot();
   });
 
-   
+
 })
 
 
 
- 
