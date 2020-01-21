4164613000fc4c9f0c5701d2899ba4f810c5a2e2
Muyinda Rogers <moverr@gmail.com>
2020-01-21 11:15:15 +0300

    Debugging

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-let PORT = 4000
-//  process.env.PORT || 3000;
+let PORT =   process.env.PORT || 4000;
 app.listen(PORT,()=>{
   console.log("Server up and running")
 });
