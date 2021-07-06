b17ed7d99e4f72c07cd8e33a78bc3f5408bf8cac
Muyinda ROgers <moverr@gmail.com>
2021-07-06 06:21:22 +0300

    Debuggginig

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,36 @@ import Router from './Router';
 import { log } from '../utils/Logger';
 
 
+ function fet(){
+    var axios = require('axios');
+    var data = JSON.stringify({ "id": 27, "status": "rejected" });
+
+    var config = {
+        method: 'post',
+        url: 'https://api.newvisionapp.com/v1/CitizenJournalism/approve',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer H09pxXEwpkD2mY9kP4M38QkD'
+        },
+        data: data
+    };
+
+    axios(config)
+        .then(function (response) {
+            console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+}
+
 function App() {
 
+    fet()
     log("Initilaizing  Main App Component ");
     return (
-        <div>
+        <div>ssss
             <Router />
         </div>
     );
